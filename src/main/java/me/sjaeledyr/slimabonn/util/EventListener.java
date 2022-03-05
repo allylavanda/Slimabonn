2df6a88f8f2bf7362cfa6f51da3cd0c37f7f5a90
Alison Williams <allylavanda@aol.com>
2022-03-04 20:11:34 -0500

    created BlockPlaceEvent
    created BlockPlaceEvent to cancel Ice Block placement on Cauldron use.

--- src/main/java/me/sjaeledyr/slimabonn/util/EventListener.java
+++ src/main/java/me/sjaeledyr/slimabonn/util/EventListener.java
@@ -6,17 +6,22 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class EventListener implements Listener {
     private Main main = Main.getPlugin(Main.class);
+
+    // Initialize global variables.
     int magma = 0;
     int ice = 0;
     boolean isCauldron = false;
+    boolean icePlaced = false;
 
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
+        // Initialize local variables.
         Player p = e.getPlayer();
         ItemStack inHand = p.getInventory().getItemInMainHand();
         Block block = e.getClickedBlock();
@@ -24,20 +29,25 @@ public class EventListener implements Listener {
             isCauldron = true;
         }
 
+        // Magma Cream interaction.
         if(inHand.getType() == Material.MAGMA_CREAM
-           && isCauldron){
+                && isCauldron){
             p.getInventory().setItemInMainHand(null);
             p.updateInventory();
             p.sendMessage("You place the magma in the cauldron.");
             magma = 1;
         }
-        if(inHand.getType() == Material.ICE
-           && isCauldron){
+
+        // Ice interaction.
+        if(inHand.getType() == Material.ICE && isCauldron){
             p.getInventory().remove(Material.ICE);
             p.updateInventory();
             p.sendMessage("You place the ice into the cauldron.");
+            icePlaced = true;
             ice = 1;
         }
+
+        // If player used a magma cream and an ice block, create slime ball.
         if(magma == 1 && ice == 1){
             p.sendMessage("You have turned the magma cream into a slimeball!");
             int emptySlot = p.getInventory().firstEmpty();
@@ -45,6 +55,15 @@ public class EventListener implements Listener {
             magma = 0;
             ice = 0;
             isCauldron = false;
+        }
+    }
+    // Cancel Ice Block Placement on Cauldron right click.
+    @EventHandler
+    public void BlockPlace(BlockPlaceEvent e) {
+        Player p = e.getPlayer();
+        if(icePlaced){
+            e.setCancelled(true);
+            icePlaced = false;
         }
     }
 }
